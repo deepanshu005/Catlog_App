b1c1bd065d10216dda4bfe7bdf895d81bba30f10
Deepanshu <deepanshurajput005@gmail.com>
2022-07-21 12:02:57 +0530

    Day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class Item {
+  final String? id;
+  final String? name;
+  final String? desc;
+  final num? price;
+  final String? color;
+  final String? image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [
+  Item(
+      id: "Deepanshu005",
+      name: "Poco M2 Pro",
+      desc: "Sexy looks with Punch hole ",
+      price: 13999,
+      color: "#33505a",
+      image:
+          " https://images-eu.ssl-images-amazon.com/images/I/41ZcSXaHLYL._SX300_SY300_QL70_FMwebp_.jpg"),
+];
