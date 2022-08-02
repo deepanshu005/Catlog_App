a195e479e620b87df1bf474c609456275241b3f4
Deepanshu <deepanshurajput005@gmail.com>
2022-08-02 10:01:02 +0530

    Day 23 completed

---
+++ lib/models/cart.dart
@@ -0,0 +1,31 @@
+import 'package:CATALOG/models/catalog.dart';
+
+class CartModel {
+  CatalogModel _catalog;
+  //Collection of Ids - store ids of each item
+  final List<int> _itemIds = [];
+
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  //get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  //Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price!);
+
+  // Add item
+  void add(Item item) {
+    _itemIds.add(item.id!);
+  }
+
+  // Remove item
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -14,11 +14,11 @@ class CatalogModel {
     )
   ];
   //get by id
-  static Item getById(int id) =>
+  Item getById(int id) =>
       items.firstWhere((element) => element.id == id, orElse: null);
 
   //get item by position
-  static Item getByPosition(int pos) => items[pos];
+  Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
